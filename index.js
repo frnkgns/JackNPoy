1da0600595699894aaf9b21ea2607d2ad4229c51
Frank Gines <frnkgines@gmail.com>
2025-03-13 13:44:47 +0800

    Adding Animation and Audio, Assigning key to play rather than loading the website it seld

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
+var NotPlayedYet = true;
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        if (NotPlayedYet) {
+            Play();
+            NotPlayedYet = false; // Update flag
+        } else {
+            setTimeout(Play, 1500); // Pass function reference (without `()`)
+        }
+    }
+});
+
+
+function Play(){
     // player 1
     let imgDice = document.querySelectorAll("img");
 
     const Player1Dice = Math.round(Math.random() * 5) + 1;
     const Player2Dice = Math.round(Math.random() * 5) + 1;
-    console.log("Player 1: ", Player1Dice);
-    console.log("Player 2: ", Player2Dice);
     
-    document.querySelector("h1").textContent = (Player1Dice > Player2Dice) ? "Player 1 Wins" : (Player2Dice > Player1Dice) ? "Player 2 Wins" : "Draw";
+    var imgDice1 = new ThrowDice(Player1Dice);
+    var imgDice2 = new ThrowDice(Player2Dice);
+        
+    function ThrowDice(DiceNumber, draw){
+        this.DiceNumber = (DiceNumber == 1) ? "diceImage/d1.png" : 
+        (DiceNumber == 2) ? "diceImage/d2.png" : 
+        (DiceNumber == 3) ? "diceImage/d3.png" :
+        (DiceNumber == 4) ? "diceImage/d4.png" :
+        (DiceNumber == 5) ? "diceImage/d5.png" :
+        "diceImage/d6.png";
+
+        imgDice[0].classList.add("spin-active1");
+        imgDice[1].classList.add("spin-active2");
+        new Audio((Player1Dice == Player2Dice) ? "sounds/draw.mp3" : "sounds/victory_6.mp3").play();
+        setTimeout(RemoveAni, 2000);
+
+        function RemoveAni(){
+            imgDice[0].classList.remove("spin-active1");
+            imgDice[1].classList.remove("spin-active2");
+        }
+    }
+    
     document.querySelector("h1").style.color = "white";
+    document.querySelector("h1").textContent = 
+        (Player1Dice > Player2Dice) ? "🚩 Player 1 Wins" : 
+        (Player2Dice > Player1Dice) ? "Player 2 Wins 🚩"  : "Draw";
+    
+    
+    imgDice[0].src = imgDice1.DiceNumber;
+    imgDice[1].src = imgDice2.DiceNumber;
 
-    imgDice[0].src = (Player1Dice == 1) ? "diceImage/d1.png" : 
-                    (Player1Dice == 2) ? "diceImage/d2.png" : 
-                    (Player1Dice == 3) ? "diceImage/d3.png" :
-                    (Player1Dice == 4) ? "diceImage/d4.png" :
-                    (Player1Dice == 5) ? "diceImage/d5.png" :
-                    "diceImage/d6.png";
-
-    imgDice[1].src = (Player2Dice == 1) ? "diceImage/d1.png" : 
-                    (Player2Dice == 2) ? "diceImage/d2.png" : 
-                    (Player2Dice == 3) ? "diceImage/d3.png" :
-                    (Player2Dice == 4) ? "diceImage/d4.png" :
-                    (Player2Dice == 5) ? "diceImage/d5.png" :
-                    "diceImage/d6.png";
-
-    // console.log();
-
-
-});
+    
+   document.querySelector("h3").textContent = "Hit Enter to Play Again"
+}
